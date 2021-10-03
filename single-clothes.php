f198dbd296ead13256ddbd1d9a8c3368b69c934b
Yury S <yury_slobodyan@ua.fm>
2021-10-03 23:38:33 +0300

    understamp child theme

---
+++ single-clothes.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+
+	<div class="wrapper" id="single-wrapper">
+
+		<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+			<div class="row">
+
+				<main class="site-main" id="main">
+
+					<?php
+					while ( have_posts() ) {
+						the_post();
+						get_template_part( 'loop-templates/content', 'single' );
+
+						// If comments are open or we have at least one comment, load up the comment template.
+						if ( comments_open() || get_comments_number() ) {
+							comments_template();
+						}
+					}
+					?>
+
+				</main><!-- #main -->
+
+			</div><!-- .row -->
+
+		</div><!-- #content -->
+
+	</div><!-- #single-wrapper -->
+
+<?php
+get_footer();
+
